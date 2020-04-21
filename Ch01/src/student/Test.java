433054b70678d292beb258ef4bca74923147a5a6
zhm200731 <1311399572@qq.com>
2020-04-21 09:33:55 +0800

    Change-Id: I2512f81dc4be925b39d74b7e7602ab53019e7a85

---
+++ Ch01/src/student/Test.java
@@ -0,0 +1,13 @@
+package student;//必须是第一条语句
+
+import teacher.Person;
+import java.io.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Person p=new Person("李宏",23);
+		p.printInfo();
+	}
+
+}
